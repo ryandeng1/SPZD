d59893b81c22e367106d2f7eacd0b1c8c11e9752
Ubuntu <ubuntu@ip-172-31-28-121.us-west-2.compute.internal>
2018-11-07 06:00:44 +0000

    Fix bug

--- ExternalIO/admm-client.cpp
+++ ExternalIO/admm-client.cpp
@@ -273,7 +273,6 @@ int main(int argc, char** argv) {
     int numShift = 20;
 
     string host_names[] = {"ec2-52-39-162-238.us-west-2.compute.amazonaws.com", "ec2-34-223-215-198.us-west-2.compute.amazonaws.com", "ec2-23-20-124-131.compute-1.amazonaws.com", "ec2-52-73-142-253.compute-1.amazonaws.com"};
-    string host_name = argv[2];
 
     if (argc < 1) {
         cout << "Please provide client id" << endl;
